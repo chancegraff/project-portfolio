a696a0e9ed1997b792a90ca6288bb24331484395
Chance Graff <cgraffrun@gmail.com>
2019-09-26 12:25:22 -0500

    Implement configured setup

---
+++ src/App/index.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import App from './index';
+
+
+it('renders without crashing', () => {});
---
+++ src/App/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './index.module.scss';
+
+const App: React.FC<{}> = () => {
+  return <div>Hello world</div>;
+};
+
+export default App;
