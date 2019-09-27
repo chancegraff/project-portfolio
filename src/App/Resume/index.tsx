67addc45908a3b065dc0c773289b42e56bae1598
Chance Graff <cgraffrun@gmail.com>
2019-09-27 14:08:13 -0500

    Implement svgs for other pages

--- src/App/Resume/index.tsx
+++ src/App/Resume/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Hero from '__components__/Hero';
 
+import { ReactComponent as ResumeSvg } from './hero.svg';
+
 import styles from './index.module.scss';
 
 const Resume: React.FC<{}> = () => {
   return (
     <div className={styles['resume-container']}>
       <Hero>
-        Resume
+        <ResumeSvg className={styles['hero-svg']} />
       </Hero>
       <div className={styles['about-container']}>
         <span>About</span>
--- src/App/Sandbox/index.tsx
+++ src/App/Sandbox/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Hero from '__components__/Hero';
 
+import { ReactComponent as SandboxSvg } from './hero.svg';
+
 import styles from './index.module.scss';
 
 const Sandbox: React.FC<{}> = () => {
   return (
     <div className={styles['sandbox-container']}>
       <Hero>
-        Sandbox
+        <SandboxSvg className={styles['hero-svg']} />
       </Hero>
       <div className={styles['about-container']}>
         <span>About</span>
