3d392cceb6e98e804d05af7cc5171563895d5bf8
Chance Graff <cgraffrun@gmail.com>
2019-09-30 11:57:49 -0500

    Fix server static files

---
+++ src/schema.js
@@ -0,0 +1,5 @@
+const { gql } = require('apollo-server');
+
+const typeDes = gql``;
+
+module.exports = typeDefs;
--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
 const port = process.env.PORT || 5000;
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,16 +13,10 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({
-    express: 'Hello from express',
-  });
-});
-
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
