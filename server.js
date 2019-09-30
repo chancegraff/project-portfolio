ea3db1dae74f0056213d3c3a820d16a603764e36
Chance Graff <cgraffrun@gmail.com>
2019-09-30 11:41:42 -0500

    Fix server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -5,14 +6,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 
 app.get('/api/hello', (req, res) => {
   res.send({
-    express: 'Hello from express'
+    express: 'Hello from express',
   });
 });
 
@@ -20,7 +22,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'public')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  })
+  });
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
