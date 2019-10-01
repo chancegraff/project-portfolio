a461400dcb4ca591b0d55be02f40cfe9e8660866
Chance Graff <cgraffrun@gmail.com>
2019-10-01 13:56:32 -0500

    Implement sort order for gql

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,13 +3,20 @@ const { UserInputError } = require('apollo-server');
 module.exports = {
   Query: {
     projects: (parent, args, { database }, info) => {
-      return database.Project.findAll();
+      return database.Project.findAll({
+        order: [
+          [
+            'createdAt',
+            'DESC',
+          ],
+        ],
+      });
     },
     project: (parent, { slug }, { database }, info) => {
       return database.Project.findOne({
         where: {
-          slug
-        }
+          slug,
+        },
       });
     },
   },
