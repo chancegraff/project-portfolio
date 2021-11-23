3bad44baf79567067bc35152a8740d999abc4e14
Chance Graff <8976922+chancegraff@users.noreply.github.com>
2021-11-23 13:13:06 -0600

    Update Medium and Dribbble URLs

--- src/App/Template/Footer/index.tsx
+++ src/App/Template/Footer/index.tsx
@@ -16,8 +16,8 @@ const Footer: React.FC<{}> = (props) => (
     <div className={styles['social-container']}>
       <SocialIcon className={styles['social-link']} url="https://github.com/chancegraff" />
       <SocialIcon className={styles['social-link']} url="https://www.linkedin.com/in/chancegraff/" />
-      <SocialIcon className={styles['social-link']} url="https://medium.com/@cgraffrun" />
-      <SocialIcon className={styles['social-link']} url="https://dribbble.com/cgraffrun" />
+      <SocialIcon className={styles['social-link']} url="https://medium.com/@chancegraff" />
+      <SocialIcon className={styles['social-link']} url="https://dribbble.com/chancegraff" />
     </div>
     <Logo className={styles['logo']} />
   </footer>
